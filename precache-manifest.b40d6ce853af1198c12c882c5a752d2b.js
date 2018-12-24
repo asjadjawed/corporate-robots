f00bf6d633851433eaf61db034a5b0d3b2992a27
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2018-12-24 14:37:16 +0500

    Updates

---
+++ precache-manifest.b40d6ce853af1198c12c882c5a752d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "860a97025d81569a6f46",
+    "url": "/corporate-robots/static/js/runtime~main.860a9702.js"
+  },
+  {
+    "revision": "153c9de179d0b86114b9",
+    "url": "/corporate-robots/static/js/main.153c9de1.chunk.js"
+  },
+  {
+    "revision": "6f2068dff06d22b5d2e3",
+    "url": "/corporate-robots/static/js/1.6f2068df.chunk.js"
+  },
+  {
+    "revision": "153c9de179d0b86114b9",
+    "url": "/corporate-robots/static/css/main.1eff7ec3.chunk.css"
+  },
+  {
+    "revision": "5c92f6a010db8ecccaaa35e6672c45a3",
+    "url": "/corporate-robots/index.html"
+  }
+];
